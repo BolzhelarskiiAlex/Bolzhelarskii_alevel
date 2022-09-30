82c8e600382585d9df3f268aa1fb5d1df9542578
BolzhelarskiiAlex <3convicted3@gmail.com>
2022-09-30 21:17:08 +0300

    add package to Task2

--- src/ua/bolzhelarskii/hw2/Task2.java
+++ src/ua/bolzhelarskii/hw2/Task2.java
@@ -1,5 +1,8 @@
-public class Trian {
+package ua.bolzhelarskii.hw2;
+
+public class Task2 {
     public static void main(String[] args) {
+
         System.out.println("Area of a triangle is " +area());
     }
 
