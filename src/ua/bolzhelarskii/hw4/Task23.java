859017a751fbb3421e57c74f56c5c3bac04afcad
BolzhelarskiiAlex <3convicted3@gmail.com>
2022-10-03 17:17:33 +0300

    выводит совсем не то...понимаю,где ошибся,но на какие значения менять не догадался..

--- src/ua/bolzhelarskii/hw4/Task23.java
+++ src/ua/bolzhelarskii/hw4/Task23.java
@@ -5,13 +5,12 @@ import java.util.Random;
 
 public class Task23 {
     public static void main(String[] args) {
-        int[] randomNum = new int[5];
+        int[] randomNum = new int[10];
         fillArray(randomNum);
         System.out.println(Arrays.toString(randomNum));
-        System.out.println("Prime num is " + countPrimes(randomNum));
-        System.out.println("Composite num is ");
+        System.out.println("Prime num is " + (Arrays.toString(countPrimes(randomNum))));
+        System.out.println("Composite num is " + (Arrays.toString(countPrimes(randomNum))));
     }
-
     private static void fillArray(int[] randomNum) {
         Random random = new Random();
         for (int i = 0; i < randomNum.length; i++) {
@@ -20,20 +19,20 @@ public class Task23 {
     }
 
     public static int[] countPrimes(int[] randomNum) {
-        for (int i = 2; i <= randomNum.length; i++) {
-            boolean isPrime = true;
-
-            for (int j = 2; j < i; j++) {
-                if (i % j == 0) {
-                    isPrime = false;
-                    break;
-                }
+        int temp;
+        int[] num = randomNum;
+        boolean isPrime=true;
+        for (int i=2; i<= randomNum.length/2; i++) {
+            temp = randomNum.length % i;
+            if (temp == 0) {
+                isPrime = false;
+                break;
             }
+        }
+        if(isPrime) {
 
-            if (isPrime) {
-                int count = i;
-                count++;
-            }
+        } else {
+
         }
         return randomNum;
     }
