97377fd1888037071d28a65af2bf11586c927fa7
BolzhelarskiiAlex <3convicted3@gmail.com>
2022-10-17 16:53:09 +0300

    switch change and copy of arrays

--- src/main/java/ua/bolzhelarskii/hw4/Task4.java
+++ src/main/java/ua/bolzhelarskii/hw4/Task4.java
@@ -8,8 +8,9 @@ public class Task4 {
         int[] randomNum = new int[2000];
         fillArray(randomNum);
         System.out.println(Arrays.toString(randomNum));
+        System.out.println(Arrays.toString(arrayCopy(randomNum)));
         System.out.println(Arrays.toString(copyFullArrayUsingClone(randomNum)));
-        System.out.println(Arrays.toString(randomNum));
+
     }
 
 
@@ -19,12 +20,15 @@ public class Task4 {
             randomNum[i] = random.nextInt(10) + 1;
         }
     }
+    public static int[] arrayCopy(int[] initialArray){
+        return  Arrays.copyOf(initialArray, initialArray.length);
+    }
     private static int[] copyFullArrayUsingClone(int[] randomNum) {
         for (int i = 0; i < randomNum.length; i++) {
             if (randomNum[i] % 2 == 0){
                 randomNum[i] = 0;
             }
         }
-        return randomNum.clone();
+        return randomNum;
     }
 }
