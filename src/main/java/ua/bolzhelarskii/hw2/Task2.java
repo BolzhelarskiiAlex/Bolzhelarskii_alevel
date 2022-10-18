5a64712e49ead2ad66b7828218e7b392a5ca7206
BolzhelarskiiAlex <3convicted3@gmail.com>
2022-10-18 11:47:21 +0300

    edited.hope now it works

--- src/main/java/ua/bolzhelarskii/hw2/Task2.java
+++ src/main/java/ua/bolzhelarskii/hw2/Task2.java
@@ -2,17 +2,16 @@ package ua.bolzhelarskii.hw2;
 
 public class Task2 {
     public static void main(String[] args) {
-        int x = 0;
-        System.out.println("Area of a triangle is " + area(x));
-    }
-
-    private static int area(int x) {
         int x1 = 1;
         int x2 = 3;
         int x3 = 6;
         int y1 = 5;
         int y2 = 5;
         int y3 = 7;
+        System.out.println("Area of a triangle is " + area(x1, x2, x3, y1, y2, y3));
+    }
+
+    private static int area(int x1, int x2, int x3, int y1, int y2, int y3) {
         int S = ((x1 - x3) * (y2 - y3) - (x2 - x3) * (y1 - y3)) / 2;
         return S;
     }
