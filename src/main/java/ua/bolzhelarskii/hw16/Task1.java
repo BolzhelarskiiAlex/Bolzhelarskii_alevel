8ce17fc8ddeaf6836531904273123e0294cd2701
BolzhelarskiiAlex <3convicted3@gmail.com>
2022-11-18 13:36:05 +0200

    edited

--- src/main/java/ua/bolzhelarskii/hw16/Task1.java
+++ src/main/java/ua/bolzhelarskii/hw16/Task1.java
@@ -7,14 +7,14 @@ import java.util.Map;
 
 public class Task1 {
     public static void main(String[] args) {
-        String text = "Для подключения нужно подать питание на колонку."+
-        "Ей понадобится некоторое время для загрузки, после чего на верхней "+
-        "части загорится светодиодная подсветка. После этого колонка сразу обнаруживается "+
-        "в сети как ChromeCast устройство, которое сразу предлагается добавить в Google Home."+
-        "Для этого надо указать в какой дом, если вдруг у вас их несколько, нужно его добавить";
-        List<String> list = Arrays.asList(text.replaceAll("[^ a-zA-Zа-яА-Я]", " ").split(" +"));
+        String text = "Для подключения нужно подать питание на колонку. " +
+        "Ей понадобится некоторое время для загрузки, после чего на верхней " +
+        "части загорится светодиодная подсветка. После этого колонка сразу обнаруживается " +
+        "в сети как ChromeCast устройство, которое сразу предлагается добавить в Google Home.";
+        List<String> list = Arrays.asList(text.replaceAll("^ А-Яа-яA-Za-z", " ").split(" +"));
         Map<String, Integer> hashMap = ListToMap(list);
         System.out.println(hashMap);
+        //не пойму,почему слова в разнобой идут
     }
     static Map<String, Integer> ListToMap(List<String> list) {
         if (list == null) {
