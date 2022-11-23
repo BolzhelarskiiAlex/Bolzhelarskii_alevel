846e8fd4b947f2ab6491208b4918c94e62618238
BolzhelarskiiAlex <3convicted3@gmail.com>
2022-11-23 13:08:45 +0200

    new

--- src/main/java/ua/bolzhelarskii/hw18/Task2/Task2.java
+++ src/main/java/ua/bolzhelarskii/hw18/Task2/Task2.java
@@ -6,6 +6,13 @@ import java.util.IntSummaryStatistics;
 import java.util.List;
 
 public class Task2 {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        Collections.addAll(numbers, 5, 2, 1, 9, 7, 10, 3, 9, 25,
+                1, 6, 8, 4, -13, -60, -13, -1, 20, -36, -123);
+        printNumberStatistic(numbers);
+    }
+
     public static void printNumberStatistic(List<Integer> list) {
         IntSummaryStatistics statistics = list.stream()
                 .filter(x -> x >= 0)
@@ -14,10 +21,5 @@ public class Task2 {
         System.out.println(statistics);
     }
 
-    public static void main(String[] args) {
-        List<Integer> numbers = new ArrayList<>();
-        Collections.addAll(numbers, 5, 2, 1, 9, 7, 10, 3, 9, 25,
-                1, 6, 8, 4, -13, -60, -13, -1, 20, -36, -123);
-        printNumberStatistic(numbers);
-    }
+
 }
