5ea9b26570ce6b0cd6422475a505989dc661c2cc
BolzhelarskiiAlex <3convicted3@gmail.com>
2022-11-24 18:01:53 +0200

    removal to a separate method

--- src/main/java/ua/bolzhelarskii/hw18/Task4/Task4.java
+++ src/main/java/ua/bolzhelarskii/hw18/Task4/Task4.java
@@ -6,8 +6,12 @@ import java.util.stream.Stream;
 
 public class Task4 {
     public static void main(String[] args) {
-        List<Integer> list = Stream.generate(() -> new Random().nextInt(200) - 100)
+        List<Integer> numbers = Stream.generate(() -> new Random().nextInt(200) - 100)
                 .limit(10).toList();
-        System.out.println("min value = " + list.stream().reduce(Integer.MAX_VALUE, Integer::min));
+        System.out.println("min value = " + minList(numbers));
+    }
+    public static int minList(List<Integer> list){
+        return list.stream()
+                .reduce(Integer.MAX_VALUE,(i,x) -> i < x ? i :  x);
     }
 }
