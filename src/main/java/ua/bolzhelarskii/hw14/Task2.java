32ec6a4ae08b68d2b41360e9ed733527de5b7801
BolzhelarskiiAlex <3convicted3@gmail.com>
2022-12-12 12:30:48 +0200

    исправлено

--- src/main/java/ua/bolzhelarskii/hw14/Task2.java
+++ src/main/java/ua/bolzhelarskii/hw14/Task2.java
@@ -7,21 +7,21 @@ public class Task2 {
         Integer[] myArray1 = new Integer[]{1, 2, 3, 4};
         Double[] myArray2 = new Double[]{1.11, 1.23, 12.34, 567.890};
         Character[] myArray3 = new Character[]{'1', 'a', 'b', 'Z'};
-        Utility.getAnyVariable(true);
-        Utility.getAnyVariable(0.50);
-        Utility.getAnyVariable("true");
-        Utility.getAnyVariable('a');
-        Utility.getAnyArray(myArray1);
-        Utility.getAnyArray(myArray2);
-        Utility.getAnyArray(myArray3);
+        Utility.printAnyVariable(true);
+        Utility.printAnyVariable(0.50);
+        Utility.printAnyVariable("true");
+        Utility.printAnyVariable('a');
+        Utility.printAnyArray(myArray1);
+        Utility.printAnyVariable(myArray2);
+        Utility.printAnyArray(myArray3);
     }
 
     public static class Utility {
-        public static <T> void getAnyVariable(T anyVariable) {
+        public static <T> void printAnyVariable(T anyVariable) {
             System.out.println(anyVariable);
         }
 
-        public static <T> void getAnyArray(T[] array) {
+        public static <T> void printAnyArray(T[] array) {
             System.out.println(Arrays.toString(array));
         }
     }
