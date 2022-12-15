88db94bef3bd4bba71f176151d00c9b39f55c499
BolzhelarskiiAlex <112811431+BolzhelarskiiAlex@users.noreply.github.com>
2022-12-15 15:28:14 +0200

    Delete src/main/resources/sql/hw22 directory

--- src/main/resources/sql/hw22/Task2.sql
+++
@@ -1,8 +0,0 @@
-create database alevel;
-create Table Customer (
-  login INT NOT NULL PRIMARY KEY,
-  firstName VARCHAR(45),
-  lastName VARCHAR(45),
-  dateOfBirth DATE,
-  country VARCHAR(45),
-  sex enum('male','female') DEFAULT 'male');
